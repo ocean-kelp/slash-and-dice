600c153cfd31436d6fa619d1424606c36b9085b2
Ronald Pardo <koren.yin@outlook.es>
2025-12-26 17:30:42 -0500

    feat: add new currency types and their associated icons

--- app/data/currencies/data.ts
+++ app/data/currencies/data.ts
@@ -19,4 +19,44 @@ export const CURRENCIES_DATA = [
     name: "Heart",
     icon: "/game/data/currencies/heart.png",
   },
+  {
+    id: "gold",
+    name: "Gold",
+    icon: "/game/data/currencies/gold.png",
+  },
+  {
+    id: "prayerstone",
+    name: "Prayerstone",
+    icon: "/game/data/currencies/prayerstone.png",
+  },
+  {
+    id: "pet-coupon",
+    name: "Pet Coupon",
+    icon: "/game/data/currencies/pet-coupon.png",
+  },
+  {
+    id: "boss-equipment-exchange-ticket",
+    name: "Boss Equipment Exchange Ticket",
+    icon: "/game/data/currencies/boss-equipment-exchange-ticket.png",
+  },
+  {
+    id: "skin-exchange-ticket",
+    name: "Skin Exchange Ticket",
+    icon: "/game/data/currencies/skin-exchange-ticket.png",
+  },
+  {
+    id: "weapon-exchange-ticket",
+    name: "Weapon Exchange Ticket",
+    icon: "/game/data/currencies/weapon-exchange-ticket.png",
+  },
+  {
+    id: "mileage-point",
+    name: "Mileage Point",
+    icon: "/game/data/currencies/mileage-point.png",
+  },
+  {
+    id: "multiplier-coupon",
+    name: "Multiplier Coupon",
+    icon: "/game/data/currencies/multiplier-coupon.png",
+  },
 ] as const;
